feca75ccb62a55dffc534e0390ef45b599177765
seasons986 <annalin12345@gmail.com>
2019-09-22 16:55:05 -0700

    Added trailing slash to /hello and /echo for increased access to page, and added correct way to get query params in url

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route('/hello')
+@app.route('/hello/')
 def hello():
-	return jsonify('Hello world!')
+	return 'Hello world!'
 
-@app.route('/echo/<key>')
-def get(key):
-	return jsonify(key)
+@app.route('/echo/')
+def getWithMsg():
+	if not request.args.get('msg'):
+		return ''
+	return request.args.get('msg')
 
 app.run(port=8080, debug=True)
